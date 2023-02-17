331cce69413d90e75eca00b9aa595438a6039fcf
Vadim <vadimshdrn@gmail.com>
2023-02-17 21:36:04 +0600

    Add function for display clear

--- calc.js
+++ calc.js
@@ -1,3 +1,15 @@
 let firstOperand = '';
 let secondOperand = '';
 let operator = '';
+
+const numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const acctions = ['-', '+', '*', '/'];
+
+const output = document.querySelector('.output');
+
+function clear() {
+    firstOperand = '';
+    secondOperand = '';
+    operator = '';
+    output.textContent = 0;
+}
